71cedf04817318b73cbc20003aabdbd9d5c0d193
Subrata Rudra <77232812+Subrata-Rudra@users.noreply.github.com>
2022-05-23 00:59:29 +0530

    Update client.js
    added https

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://web-chat-by-subrata-rudra.herokuapp.com');
+const socket = io('https://web-chat-by-subrata-rudra.herokuapp.com');
 
 // Get DOM variables in respective JS variables
 const form = document.getElementById('send-container');
